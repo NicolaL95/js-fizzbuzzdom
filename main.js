f8b073305a77a15770259057900c6805f7a3428d
NicolaL95 <nicola.lilla@gmail.com>
2021-10-13 17:48:31 +0200

    Inizio lavori condizionali

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ for (let i = 1; i < 101; i++) {
     const element = `<li class="box box${i}">${i}</li>`;
 
     if (i % 3 == 0) {
-
+        let x = document.querySelector("li.box");
+        x.classList.add()
     }
 
     else if (i % 5 == 0) {
