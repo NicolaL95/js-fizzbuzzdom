188ede737e59e237834e42e8c5526b61748736c2
NicolaL95 <nicola.lilla@gmail.com>
2021-10-13 17:22:18 +0200

    Aggiunta condizionali  per i multipli

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ const ul = document.querySelector("ul.list");
 
 for (let i = 1; i < 101; i++) {
     const element = `<li class="box box${i}">${i}</li>`;
+
+    if (i % 3 == 0) {
+
+    }
+
+    else if (i % 5 == 0) {
+
+    }
+
+    else if (i % 3 == 0 && i % 5 == 0) {
+
+    }
+
+    else {
+
+    }
     ul.innerHTML += element;
 }
 
