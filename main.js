a198d8eeb8f4ef74d83f26731442d6069de94eec
NicolaL95 <nicola.lilla@gmail.com>
2021-10-13 18:27:19 +0200

    Continuazione lavori

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 const ul = document.querySelector("ul.list");
 
 for (let i = 1; i < 101; i++) {
-    const element = `<li class="box box${i}">${i}</li>`;
-
+    let element;
+    let x = document.querySelector("li.box");
+    console.log(i);
     if (i % 3 == 0) {
-        let x = document.querySelector("li.box");
-        x.classList.add()
+
+        x.classList.add("fizz");
+        element = `<li class="box box${i}">Fizz</li>`;
+        console.log("Fizz");
     }
 
     else if (i % 5 == 0) {
-
+        element = `<li class="box box${i}">Buzz</li>`;
+        console.log("Fuzz");
     }
 
     else if (i % 3 == 0 && i % 5 == 0) {
-
+        element = `<li class="box box${i}">FizzBuzz</li>`;
     }
 
     else {
-
+        element = `<li class="box box${i}">${i}</li>`;
+        console.log("Else");
     }
+
     ul.innerHTML += element;
 }
 
